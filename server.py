4b23916df9f18c9cd2d081f52bb122a7adc988bb
Liron Hayon <lirhayon@gmail.com>
2021-12-11 21:38:21 -0500

    fixing aborting problem

--- server.py
+++ server.py
@@ -122,6 +122,21 @@ def home():
     return render_template("home.html")
 
 
+app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+
 if __name__ == "__main__":
 
     import click
